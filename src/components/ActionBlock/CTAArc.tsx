7276439c61beb40d9cdf5ae2bf56f53dc4f9443b
Galal Elsayed <agalal3516@gmail.com>
2026-01-07 15:25:01 +0200

    Added a arc image

--- src/components/ActionBlock/CTAArc.tsx
+++ src/components/ActionBlock/CTAArc.tsx
@@ -1,20 +1,19 @@
 "use client";
 
 import React from "react";
+import Image from "next/image";
 
 export default function CTAArc() {
   return (
-    <div className="absolute inset-x-0 top-0 h-[300px] md:h-[400px] overflow-hidden pointer-events-none select-none">
+    <div className="absolute inset-x-0 top-0 w-full pointer-events-none select-none">
       {/* The Hill Shape */}
-      <span
-        className="absolute top-0 left-1/2 -translate-x-1/2 w-[300%] md:w-[200%] aspect-[1/1] rounded-[100%] bg-black shadow-[inset_0_1px_40px_rgba(255,255,255,0.3)]"
-        style={{
-          background: 'radial-gradient(50% 50% at 50% 50%, #000 60%, #030303 85%, #050505 100%)'
-        }}
-      />
-      {/* Top rim highlight derived from box-shadow, plus a faint border */}
-      <span
-        className="absolute top-[1px] left-1/2 -translate-x-1/2 w-[300%] md:w-[200%] aspect-[1/1] rounded-[100%] border-t border-white/20 opacity-50"
+      <Image
+        src="/ActionBlock/Arc.png"
+        alt=""
+        width={1920}
+        height={1080}
+        className="w-full h-auto"
+        priority
       />
     </div>
   );
--- src/components/ActionBlock/LiveblocksCTA.tsx
+++ src/components/ActionBlock/LiveblocksCTA.tsx
@@ -6,9 +6,9 @@ import CTAButtons from "./CTAButtons";
 
 export default function LiveblocksCTA() {
   return (
-    <section className="relative overflow-hidden bg-black">
+    <section className="relative overflow-hidden bg-[#0a0a0a]">
       <CTAArc />
-      <div className="relative max-w-6xl mx-auto px-6 py-20 text-center">
+      <div className="relative max-w-6xl mx-auto px-6 py-30 md:py-42 text-center">
         <CTATitle />
         <CTAButtons />
       </div>
